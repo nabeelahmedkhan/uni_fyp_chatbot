6ca137a5e0e4b5038dcf701b42715dbc038ca198
nabeelahmedkhan <ahmednabeel147@gmail.com>
2019-10-07 18:34:54 +0500

    chatbot file

---
+++ chatbot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  7 16:17:56 2019
+
+@author: haier
+"""
+import tensorflow as tf
+import keras
+import numpy as np
+import matplotlib as plt
+import pandas as pd
